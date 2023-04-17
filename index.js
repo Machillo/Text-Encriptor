e439a296e1545c4264e7a943a2e3f901bad04067
Kenneth Alvarado <gatotico99@gmail.com>
2023-04-16 19:59:47 -0600

    Adicion del boton copiar

--- index.js
+++ index.js
@@ -22,31 +22,40 @@ function encriptar() {
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     swal("Ooops!", "Debes ingresar un texto", "warning");
     }
-  }
-  
-  function desencriptar() {
+}
+
+function desencriptar() {
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo-mensaje");
     let parrafo = document.getElementById("parrafo");
     let muñeco = document.getElementById("muñeco");
-  
+
     let textoCifrado = texto
-      .replace(/enter/gi, "e")
-      .replace(/imes/gi, "i")
-      .replace(/ai/gi, "a")
-      .replace(/ober/gi, "o")
-      .replace(/ufat/gi, "u");
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
     
-      if (texto.length != 0) {
+    if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto desencriptado con éxito";
         parrafo.textContent = "";
         muñeco.src = "img/Decrypt.jpg";
-      } else {
+    } else {
         muñeco.src = "img/oracle.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         swal("Ooops!", "Debes ingresar un texto", "warning");
-      }
-  }
-  
+    }
+}
+
+const btnCopiar = document.querySelector('#btn-copiar');
+
+btnCopiar.addEventListener('click', () => {
+    const textoCifrado = document.getElementById('texto');
+    textoCifrado.select();
+    document.execCommand('copy');
+});
+
+

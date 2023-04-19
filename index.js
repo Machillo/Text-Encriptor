455a931a118ec01ccfafc6d96016518d6fee2d27
Kenneth Alvarado <gatotico99@gmail.com>
2023-04-19 10:42:43 -0600

    Añadicion de validacion de texto ingresado

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
 function encriptar() {
-    let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("parrafo");
-    let muñeco = document.getElementById("muñeco");
+    const texto = document.getElementById("texto").value;
+    const tituloMensaje = document.getElementById("titulo-mensaje");
+    const parrafo = document.getElementById("parrafo");
+    const muñeco = document.getElementById("muñeco");
 
-    let textoCifrado = texto
-    .replace(/e/gi, "enter")
-    .replace(/i/gi, "imes")
-    .replace(/a/gi, "ai")
-    .replace(/o/gi, "ober")
-    .replace(/u/gi, "ufat");
+    if (typeof texto === 'string') {
+        const textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
 
-    if (texto.length != 0) {
-    document.getElementById("texto").value = textoCifrado;
-    tituloMensaje.textContent = "Texto encriptado con éxito";
-    parrafo.textContent = "";
-    muñeco.src = "img/Encrypt.jpg";
+        if (texto.length != 0) {
+            document.getElementById("texto").value = textoCifrado;
+            tituloMensaje.textContent = "Texto encriptado con éxito";
+            parrafo.textContent = "";
+            muñeco.src = "img/Encrypt.jpg";
+        } else {
+            muñeco.src = "img/oracle.png";
+            tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+            parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+            swal("Ooops!", "Debes ingresar un texto", "warning");
+        }
     } else {
-    muñeco.src = "img/oracle.png";
-    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-    swal("Ooops!", "Debes ingresar un texto", "warning");
-    }
+    swal("Ooops!", "El texto ingresado no es válido", "error");
+}
 }
 
 function desencriptar() {
-    let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("parrafo");
-    let muñeco = document.getElementById("muñeco");
+    const texto = document.getElementById("texto").value;
+    const tituloMensaje = document.getElementById("titulo-mensaje");
+    const parrafo = document.getElementById("parrafo");
+    const muñeco = document.getElementById("muñeco");
 
-    let textoCifrado = texto
-    .replace(/enter/gi, "e")
-    .replace(/imes/gi, "i")
-    .replace(/ai/gi, "a")
-    .replace(/ober/gi, "o")
-    .replace(/ufat/gi, "u");
-    
-    if (texto.length != 0) {
-        document.getElementById("texto").value = textoCifrado;
-        tituloMensaje.textContent = "Texto desencriptado con éxito";
-        parrafo.textContent = "";
-        muñeco.src = "img/Decrypt.jpg";
+    if (typeof texto === 'string') {
+        const textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+        
+        if (texto.length != 0) {
+            document.getElementById("texto").value = textoCifrado;
+            tituloMensaje.textContent = "Texto desencriptado con éxito";
+            parrafo.textContent = "";
+            muñeco.src = "img/Decrypt.jpg";
+        } else {
+            muñeco.src = "img/oracle.png";
+            tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+            parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+            swal("Ooops!", "Debes ingresar un texto", "warning");
+        }
     } else {
-        muñeco.src = "img/oracle.png";
-        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-        swal("Ooops!", "Debes ingresar un texto", "warning");
+        swal("Ooops!", "El texto ingresado no es válido", "error");
     }
 }
 
@@ -57,5 +65,3 @@ btnCopiar.addEventListener('click', () => {
     textoCifrado.select();
     document.execCommand('copy');
 });
-
-
